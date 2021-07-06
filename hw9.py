5c4b5303b412d57286ffd7a39b4ab0b7b3317f70
cliu268 <cliu268@gmail.com>
2021-07-05 20:33:42 -0400

    keyboard input

--- hw9.py
+++ hw9.py
@@ -1,2 +1,20 @@
 # battleship code
 print("Hello world")
+#input("Enter your input:")
+import msvcrt
+while True:
+    input_char=msvcrt.getch()
+    print(int.from_bytes(input_char, "big"))
+
+    if int.from_bytes(input_char, "big") == 72: # ascii 17
+        print("UP")
+    elif int.from_bytes(input_char, "big") == 80: #chr(18):
+        print("DOWN")
+    elif int.from_bytes(input_char, "big") == 77: #chr(19):
+        print("RIGHT")
+    elif int.from_bytes(input_char, "big") == 75: #chr(20):
+        print("LEFT")   
+    elif input_char == b' ': #chr(32):
+        print("SPACE") 
+    else:
+        print(input_char)                    
